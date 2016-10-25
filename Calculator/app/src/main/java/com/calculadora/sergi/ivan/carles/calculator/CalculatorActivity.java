5da823d8df36b83398ba9b6f6c37e2626ddc80cd
Carles Morales <carles.morales@estudiant.upc.edu>
2016-10-25 18:21:25 +0200

    Revert "Correcció i millores en els listener"
    This reverts commit 133c05575520fe3d8b410720ec73032e2dd8abd0.

--- Calculator/app/src/main/java/com/calculadora/sergi/ivan/carles/calculator/CalculatorActivity.java
+++ Calculator/app/src/main/java/com/calculadora/sergi/ivan/carles/calculator/CalculatorActivity.java
@@ -2,79 +2,12 @@ package com.calculadora.sergi.ivan.carles.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 public class CalculatorActivity extends AppCompatActivity {
-
-    private String sNum1 = "", sNum2= "",sOp="";
-    private float Resultado = 0;
-    private Button btn_clear;
-    private Button btn_equal;
-    private TextView screen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
-        btn_clear = (Button) findViewById(R.id.btn_clear);
-        btn_equal = (Button) findViewById(R.id.btn_equal);
-        screen = (TextView) findViewById(R.id.screen);
-        listen_all();
-    }
-
-    public void numberClick(View view) {
-        Button b = (Button) view;
-        CharSequence num = b.getText();
-        String numero = num.toString();
-        screen.setText(screen.getText()+ numero);
-    }
-
-    public void operationClick(View view) {
-        sNum1 = screen.getText().toString();
-        Button b = (Button) view;
-        CharSequence op = b.getText();
-        sOp = op.toString();
-        screen.setText("");
-    }
-
-    private void listen_all() {
-        btn_equal.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sNum2 = screen.getText().toString();
-                switch(sOp){
-                    case "/":
-                        Resultado = Float.parseFloat(sNum1) / Float.parseFloat(sNum2);
-                        break;
-                    case "x":
-                        Resultado = Float.parseFloat(sNum1) * Float.parseFloat(sNum2);
-                        break;
-                    case "-":
-                        Resultado = Float.parseFloat(sNum1) - Float.parseFloat(sNum2);
-                        break;
-                    case "+":
-                        Resultado = Float.parseFloat(sNum1) + Float.parseFloat(sNum2);
-                        break;
-                }
-                if (Math.round(Resultado) == Resultado) {
-                    screen.setText(String.valueOf((int)Resultado));
-                } else {
-                    screen.setText(String.valueOf(Resultado));
-                }
-                sNum2 = "";
-            }
-        });
-
-        btn_clear.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                screen.setText("");
-                sNum1 = "";
-                sNum2 = "";
-            }
-        });
     }
 }
