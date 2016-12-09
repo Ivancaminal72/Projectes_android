36c5966442958d079ef236ac4742611f87a2eacb
Ivan Caminal <ivancaminal72@gmail.com>
2016-12-09 10:43:39 +0100

    Mostro llista de grups al layout Artista

--- FinalProject/v0.1/Artist/app/src/main/java/sergi/ivan/carles/artist/ActualEventActivity.java
+++ FinalProject/v0.1/Artist/app/src/main/java/sergi/ivan/carles/artist/ActualEventActivity.java
@@ -3,6 +3,8 @@ package sergi.ivan.carles.artist;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -12,35 +14,43 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-
-import static java.lang.System.arraycopy;
 
 
 public class ActualEventActivity extends AppCompatActivity {
 
     public static final int GROUP_MAX_SIZE = 4;
     private ArrayList<Group> groups;
-    private ArrayList<Song> songList;
+    private ArrayList<Song> songs;
+    private ListView group_list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actual_event);
 
-        //Random songList generation
-        songList = new ArrayList<>();
-        for(int i=0; i<10; i++){
+        //Random songs generation
+        songs = new ArrayList<>();
+        for(int i=0; i<100; i++){
             Song importedSong = new Song(i,String.format("Cancion%d",i),String.format("Artista%d",i));
-            songList.add(importedSong);
+            songs.add(importedSong);
         }
 
         //Random voting groups generation
         groups = new ArrayList<>();
-        for(int i=0; i<2; i++){
-            Integer[] songkeys = new Integer[]{1, 2, 3, 4};
-            groups.add(new Group(String.format("Cancion%d",i),songkeys));
+        for(int i=0; i<20; i++){
+            Integer[] songkeys = new Integer[]{1+i, 2+i, 3+i, 4+i};
+            groups.add(new Group(String.format("Group to select %d",i),songkeys));
         }
+
+        //Set layout
+        group_list = (ListView) findViewById(R.id.group_list);
+        group_list.setAdapter(new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                getGroupsNames()
+        ));
+
+
 
         //Send the selected group to firebase database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
@@ -57,16 +67,24 @@ public class ActualEventActivity extends AppCompatActivity {
 
     }
 
+    private ArrayList<String> getGroupsNames() {
+        ArrayList<String> group_names = new ArrayList<>();
+        for(int i =0; i<groups.size(); i++){
+            group_names.add(groups.get(i).getName());
+        }
+        return group_names;
+    }
+
     private String toJson(int index) {
         ArrayList <String> sNames = new ArrayList<>();
         ArrayList <String> sArtists = new ArrayList<>();
         Integer [] songsIds;
         songsIds = groups.get(index).getSongids();
         for(int i=0; i<GROUP_MAX_SIZE; i++){
-            for(int j=0; j<songList.size(); j++){
-                if(songList.get(j).getSongId()==songsIds[i]){
-                    sNames.add(songList.get(j).getName());
-                    sArtists.add(songList.get(j).getArtist());
+            for(int j = 0; j< songs.size(); j++){
+                if(songs.get(j).getSongId()==songsIds[i]){
+                    sNames.add(songs.get(j).getName());
+                    sArtists.add(songs.get(j).getArtist());
                 }
             }
         }
