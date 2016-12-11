794c6f13388214c2c4d00b2da99fd764a7e4201d
Carles Morales <carles.morales@estudiant.upc.edu>
2016-12-11 04:20:03 +0100

    Principi de layout de Client

--- FinalProject/v0.1/Client/app/src/main/java/sergi/ivan/carles/client/ActualEventActivity.java
+++ FinalProject/v0.1/Client/app/src/main/java/sergi/ivan/carles/client/ActualEventActivity.java
@@ -1,8 +1,18 @@
 package sergi.ivan.carles.client;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -16,6 +26,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ActualEventActivity extends AppCompatActivity {
 
@@ -28,6 +39,8 @@ public class ActualEventActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.actual_event_activiy);
+        ListView lv = (ListView) findViewById(R.id.vote_list);
+        //lv.setAdapter(new MyListAdaper(this, R.layout.list_item, data));
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference act_ref = database.getReference("act_group");
@@ -65,5 +78,44 @@ public class ActualEventActivity extends AppCompatActivity {
 
         //TODO: quan cliques enviar votació
 
+    }
+
+    private class MyListAdaper extends ArrayAdapter<String> {
+        private int layout;
+        private List<String> mObjects;
+        private MyListAdaper(Context context, int resource, List<String> objects) {
+            super(context, resource, objects);
+            mObjects = objects;
+            layout = resource;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            ViewHolder mainViewholder = null;
+            if(convertView == null) {
+                LayoutInflater inflater = LayoutInflater.from(getContext());
+                convertView = inflater.inflate(layout, parent, false);
+                ViewHolder viewHolder = new ViewHolder();
+                viewHolder.thumbnail = (ImageView) convertView.findViewById(R.id.list_item_thumbnail);
+                viewHolder.title = (TextView) convertView.findViewById(R.id.list_item_text);
+                viewHolder.button = (Button) convertView.findViewById(R.id.list_item_btn);
+                convertView.setTag(viewHolder);
+            }
+            mainViewholder = (ViewHolder) convertView.getTag();
+            mainViewholder.button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(getContext(), "Button was clicked for list item " + position, Toast.LENGTH_SHORT).show();
+                }
+            });
+            mainViewholder.title.setText(getItem(position));
+
+            return convertView;
+        }
+    }
+    public class ViewHolder {
+        ImageView thumbnail;
+        TextView title;
+        Button button;
     }
 }
