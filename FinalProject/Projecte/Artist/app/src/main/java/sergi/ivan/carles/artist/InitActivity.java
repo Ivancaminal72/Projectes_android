a06ca43c31c0f69f7e620a482c71ffe0639dbf72
Ivan Caminal <ivancaminal72@gmail.com>
2017-01-13 21:15:55 +0100

    [Artist] Creació classe EventAdapter, Layout InitActiviti
    Ara el sort es fa directament al adaptador en comptes de la llista "events"

--- FinalProject/Projecte/Artist/app/src/main/java/sergi/ivan/carles/artist/InitActivity.java
+++ FinalProject/Projecte/Artist/app/src/main/java/sergi/ivan/carles/artist/InitActivity.java
@@ -2,11 +2,14 @@ package sergi.ivan.carles.artist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
 
@@ -15,34 +18,83 @@ import static java.lang.System.currentTimeMillis;
 
 public class InitActivity extends AppCompatActivity {
 
+    public static final int MILLIS_DAY = 86400000;
+    public static final int MILLIS_HOUR = 3600000;
+    public static final int MILLIS_MINUTE = 60000;
     private ArrayList<Event> events;
     private ListView event_list;
+    private EventAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_init);
+        getSupportActionBar().setTitle(R.string.next_events);
 
         //Load artist events
         events = new ArrayList<>();
-        //Todo: (versió 0.5) consultar a firebase els events de l'artista
-        events.add(new Event("carnaval2", new Date(currentTimeMillis()+7000000), new Date(currentTimeMillis()+9000000), "place"));
-        events.add(new Event("carnaval1", new Date(currentTimeMillis()+4000000), new Date(currentTimeMillis()+6000000), "place"));
-        events.add(new Event("carnaval3", new Date(currentTimeMillis()+10000000), new Date(currentTimeMillis()+12000000), "place"));
+        //Todo: (versió 0.5) consultar a firebase els events de l'artista (make not chatch old events)
+        events.add(new Event("Carnaval2", new Date(currentTimeMillis()+1000000), new Date(currentTimeMillis()+1200000), "place", "room"));
+        events.add(new Event("Carnaval1", new Date(currentTimeMillis()-1000000), new Date(currentTimeMillis()+1200000), "place"));
+        events.add(new Event("Carnaval3", new Date(currentTimeMillis()+10000000), new Date(currentTimeMillis()+12000000), "place", "room"));
+        events.add(new Event("Carnaval5", new Date(currentTimeMillis()+140000000), new Date(currentTimeMillis()+150000000), "place"));
+        events.add(new Event("Carnaval6", new Date(currentTimeMillis()+200000000), new Date(currentTimeMillis()+220000000), "place"));
+        events.add(new Event("Carnaval6", new Date(currentTimeMillis()+240000000), new Date(currentTimeMillis()+340000000), "place"));
+        events.add(new Event("Carnaval8", new Date(currentTimeMillis()+1300000000), new Date(currentTimeMillis()+1400000000), "place"));
 
-        //Sort events by startDate
-        Collections.sort(events, new Comparator<Event>(){
+        //Create adapter and sort events by startDate
+        adapter = new EventAdapter();
+        adapter.sort(new Comparator<Event>(){
             public int compare(Event e1, Event e2) {
                 if (e1.getStartDate().after(e2.getStartDate())) return 1;
                 else return -1;
             }
         });
-
         event_list = (ListView) findViewById(R.id.events_list);
-        event_list.setAdapter(new ArrayAdapter<>(
-                this,
-                android.R.layout.simple_list_item_1,
-                events
-        ));
+        event_list.setAdapter(adapter);
+
+    }
+    private class EventAdapter extends ArrayAdapter<Event> {
+        EventAdapter() {
+            super(InitActivity.this, R.layout.item_events_list, events);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View result = convertView;
+            if(convertView == null) {
+                LayoutInflater inflater = getLayoutInflater();
+                result = inflater.inflate(R.layout.item_events_list, parent, false);
+            }
+            Event event = getItem(position);
+            TextView time = (TextView) result.findViewById(R.id.item_time);
+            Date initDate = event.getStartDate();
+            Long timeRemaining = initDate.getTime()-currentTimeMillis();
+            if(timeRemaining<0){
+                Date endDate = event.getEndDate();
+                Long timeToEnd = endDate.getTime()-currentTimeMillis();
+                if(timeToEnd>0){
+                    time.setTextColor(getResources().getColor(R.color.colorAccent));
+                    time.setText(R.string.now);
+                }
+            } else if(timeRemaining < MILLIS_HOUR){
+                time.setText(String.valueOf(timeRemaining/ MILLIS_MINUTE)+" m");
+            } else if(timeRemaining < MILLIS_DAY){
+                time.setText(String.valueOf(timeRemaining/ MILLIS_HOUR)+" h");
+            } else{
+                time.setText(String.valueOf(timeRemaining/ MILLIS_DAY)+" d");
+            }
+
+            TextView name = (TextView) result.findViewById(R.id.item_name);
+            name.setText(event.getName());
+
+            TextView place = (TextView) result.findViewById(R.id.item_place);
+            if(event.getRoom() != null){
+                place.setText(event.getPlace() +" - "+event.getRoom());
+            }else{
+                place.setText(event.getPlace());
+            }
+            return result;
+        }
     }
 }
