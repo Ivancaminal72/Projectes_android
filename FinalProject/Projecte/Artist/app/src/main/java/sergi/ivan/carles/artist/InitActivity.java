9715ac60300564ee2a7476f02769506ab84a2745
Carles Morales <carles.morales@estudiant.upc.edu>
2017-01-16 17:49:14 +0100

    [A] Implementat el onChildChange per a que carregui de Firebase les modificacions que es fagin als events ja existents

--- FinalProject/Projecte/Artist/app/src/main/java/sergi/ivan/carles/artist/InitActivity.java
+++ FinalProject/Projecte/Artist/app/src/main/java/sergi/ivan/carles/artist/InitActivity.java
@@ -23,6 +23,8 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
 
 import static java.lang.System.currentTimeMillis;
@@ -53,8 +55,8 @@ public class InitActivity extends AppCompatActivity {
 
         //Load upcoming artist events
         events = new ArrayList<>();
-        Query futureEvents = eventRef.orderByChild("end").startAt(currentTimeMillis(), "end");
-        showEvents(futureEvents);
+        Query queryEvents = eventRef.orderByChild("end").startAt(currentTimeMillis(), "end");
+        showEvents(queryEvents);
 
         adapter = new EventAdapter();
         event_list = (ListView) findViewById(R.id.event_listView);
@@ -68,8 +70,8 @@ public class InitActivity extends AppCompatActivity {
         });
     }
 
-    private void showEvents(Query futureEvents) {
-        ListenerDatabase = futureEvents.addChildEventListener(new ChildEventListener() {
+    private void showEvents(Query queryEvents) {
+        ListenerDatabase = queryEvents.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot eventSnapshot, String previousChildName) {
                 String key = eventSnapshot.getKey();
@@ -85,11 +87,30 @@ public class InitActivity extends AppCompatActivity {
                     Log.i("info", "event Added");
                     events.add(new Event(key,name, new Date(init), new Date(end), place));
                 }
+                sortEvents();
                 adapter.notifyDataSetChanged();
             }
 
             @Override
-            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+            public void onChildChanged(DataSnapshot eventSnapshot, String s) {
+                String key = eventSnapshot.getKey();
+                for(int i = 0; i<events.size(); i++){
+                    if(key.equals(events.get(i).getKey())){
+                        String name = eventSnapshot.child("name").getValue().toString();
+                        String place = eventSnapshot.child("place").getValue().toString();
+                        Long init = (long) eventSnapshot.child("start").getValue();
+                        Long end = (long) eventSnapshot.child("end").getValue();
+                        events.get(i).setName(name);
+                        events.get(i).setPlace(place);
+                        events.get(i).setStartDate(new Date(init));
+                        events.get(i).setEndDate(new Date(end));
+                        if (eventSnapshot.child("room").exists()) {
+                            String room = eventSnapshot.child("room").getValue().toString();
+                            events.get(i).setRoom(room);
+                        }
+                        adapter.notifyDataSetChanged();
+                    }
+                }
             }
 
             @Override
@@ -163,7 +184,6 @@ public class InitActivity extends AppCompatActivity {
                         String room = data.getStringExtra("room");
                         actRef.child(key).child("room").setValue(room);
                     }
-
                 }
                 break;
             case UPDATE_EVENT:
@@ -238,4 +258,16 @@ public class InitActivity extends AppCompatActivity {
             return result;
         }
     }
+
+    private void sortEvents() {
+        //Sort events by startDate
+        Collections.sort(events, new Comparator<Event>() {
+            @Override
+            public int compare(Event e1, Event e2) {
+                if (e1.getStartDate().after(e2.getStartDate())) return 1;
+                else return -1;
+            }
+        });
+    }
+
 }
