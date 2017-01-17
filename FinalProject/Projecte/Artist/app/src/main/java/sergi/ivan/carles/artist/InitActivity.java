9657b1870286d14bd8c47c5bfe2cedae212c293f
Carles Morales <carles.morales@estudiant.upc.edu>
2017-01-17 22:35:19 +0100

    [A] Solucionat bug.

--- FinalProject/Projecte/Artist/app/src/main/java/sergi/ivan/carles/artist/InitActivity.java
+++ FinalProject/Projecte/Artist/app/src/main/java/sergi/ivan/carles/artist/InitActivity.java
@@ -108,7 +108,7 @@ public class InitActivity extends AppCompatActivity {
             public void onChildAdded(DataSnapshot eventSnapshot, String previousChildName) {
                 String key = eventSnapshot.getKey();
                 for(int i=0; i<keys.length; i++){
-                    if(keys[i].equals(key)){
+                    if(key.equals(keys[i])){
                         String name = eventSnapshot.child("name").getValue().toString();
                         String place = eventSnapshot.child("place").getValue().toString();
                         Long init = (long) eventSnapshot.child("start").getValue();
